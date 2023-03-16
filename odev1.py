807d856d0214cf72768ac0052725179521374e09
Yasin Tonbul <77286189+yasintnbl@users.noreply.github.com>
2023-03-16 18:31:57 +0300

    Add files via upload

---
+++ odev1.py
@@ -0,0 +1,28 @@
+#Python'da Veri Tiplerini araştırınız, her bir veri tipi için kendi cümlelerinizle açıklamalar yazınız.
+# Sayısal veri tipleri
+# İnteger : Matematiksel işlemlerde kullanılan ama ondalık değil tamsayı halinde olan veri tipidir. Örnek:1,2,-2,-5
+# Float   : Matematiksel işlemlerde kullanılan ondalık sayıları da içeren veri tipidir. Örnek:3.6,8.5,4.7
+# Complex : Matematiksel işlemlerde kullanılan karmaşık yapılar oluşturabildiğin veri tipidir. Örnek: 1a,3c
+
+#Metinsel veri tipleri
+# string  :Metinsel bir veri tipidir matematiksel bir işlem yapılamaz Örnek:"Ali", "Veli"
+
+# Mantıksal veri tipleri
+# Boolean : Doğru ve yanlış değerlerini bulmak için kullanılan veri tipidir Örnek: 1=1:True, 1=2:False
+
+# Dizi veri tipleri
+# List    : Bir çok veriyi içerisinde barındırabilen veri tipidir. Örnek: x=(1,2,3,4), y=("Ali", "Veli","Ahmet")
+
+
+# Giriş ekranı için kullanılabir. 
+# Örnek:
+
+
+userEmail="yasin@gmail.com"
+userPassword="1234"
+email="yasin@gmail.com"
+password="1234"
+if password==userPassword and email==userEmail:
+    print("Giriş başarılı")
+else:
+    print("Giriş başarısız")
